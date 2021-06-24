dea8c712ed34ad34fe09c97c146e54f0e004a25d
vanderL <vnd.vander@gmail.com>
2021-06-24 16:17:23 -0300

    Creating and Styling Navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa';
+
+import { 
+    Nav, 
+    NavbarContainer, 
+    NavLogo, 
+    MobileIcon, 
+    NavItem, 
+    NavLinks, 
+    NavMenu,
+    NavBtn,
+    NavBtnLink
+} from './styles';
+
+const Navbar = () => {
+    return (    
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/'>ForteGPS</NavLogo>
+                    
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>Sobre</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='discover'>Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='services'>Nossos Serviços</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='signup'>Contrata</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    
+                    <NavBtn>
+                        <NavBtnLink to="/signin">Login</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    )
+}
+
+export default Navbar;
