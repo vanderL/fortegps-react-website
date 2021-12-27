8454496bddb455be87bcac74773d8290e8fa223a
vanderL <vnd.vander@gmail.com>
2021-12-27 13:37:48 -0300

    HttpsRedirect

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import HttpsRedirect from 'react-https-redirect';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HttpsRedirect>
+      <App />
+    </HttpsRedirect>
   </React.StrictMode>,
   document.getElementById('root')
 );
